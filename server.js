016661dce9995bab70ff580a59d78ee9597373cd
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-04-15 17:25:46 -0700

    Display all Pokemon images on the Index route

--- server.js
+++ server.js
@@ -2,13 +2,20 @@
 // Pokedex
 //////////////////////
 
+// .env
 require("dotenv").config()
 
+// Dependencies
 const express = require("express")
 const app = express()
+
+// Config
 const PORT = process.env.PORT
 
+// Database
 const pokemon = require("./models/pokemon.js")
+
+// Sub Dependencies
 const morgan = require("morgan")
 const methodOverride = require("method-override")
 
@@ -17,9 +24,17 @@ const methodOverride = require("method-override")
 // Middleware
 //////////////
 
+// Body Parser
 app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Morgan Dep
 app.use(morgan("tiny"))
+
+// Static
 app.use("/static", express.static("public"))
+
+// MethodOverride Dep
 app.use(methodOverride("_method"))
 
 
@@ -27,13 +42,31 @@ app.use(methodOverride("_method"))
 // Routes
 //////////////
 
+// INDUCES - Index, New, Delete, Update, Create, Edit, Show
+
+app.get("/", (req, res) => {
+    res.send("home is working")
+})
 
 // Index route
 
 app.get("/pokemon", (req, res) => {
-    res.render("index.ejs", {data: pokemon})
+    res.render("index.ejs", {pokeData: pokemon})
 })
 
+// New route
+
+app.get("/pokemon/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+// Delete - Note: Look up fruits app
+
+// Edit / Update route
+
+app.get("/pokemon/:id/edit", (req, res) => {
+    res.render("edit.ejs")
+})
 
 // Show route
 
@@ -42,24 +75,10 @@ app.get("/pokemon/:id", (req, res) => {
 })
 
 
-// New route
-
-app.get("/pokemon/new", (req, res) => {
-    res.render("new.ejs")
-})
-
-
-// Edit route
-
-app.get("/pokemon/:id/edit", (req, res) => {
-    res.render("edit.ejs")
-})
-
-
 //////////////////
 // Listen Route
 //////////////////
 
 app.listen(PORT, () => {
-    console.log("PORT is listening!")
+    console.log(`PORT is listening! ${PORT}`)
 })
